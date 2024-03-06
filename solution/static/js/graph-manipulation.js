44654113fccad14fe88241173d373bab15bcb11c
Olha Volianyk <voli1930@rgs.newcastle.sch.uk>
2024-03-06 11:41:59 +0000

    clear and generate buttons + graph manipulation bugs

--- solution/static/js/graph-manipulation.js
+++ solution/static/js/graph-manipulation.js
@@ -17,11 +17,53 @@ function close_to(x, y){
     return [-1,-1];
 }
 
+function clear_graph(graph_div){
+    while(graph_div.firstChild){
+        graph_div.removeChild(graph_div.firstChild);
+    }
+    coordinates.splice(0, coordinates.length);
+}
+
+function add_node(graph_div, x, y){
+    const node = document.createElement('div');
+    node.classList.add('node');
+    node.id = x + "-" + y;
+
+    node.style.left = x+'px';
+    node.style.top = y+'px';
+
+    graph_div.appendChild(node);
+    coordinates.push([x, y]);
+
+    return true;
+}
+
+function generate_cities(div, n){
+    clear_graph(div);
+
+    const containerRect = div.getBoundingClientRect();
+    const left = containerRect.left + 10;
+    const right = containerRect.right - 10;
+    const top = containerRect.top + 10;
+    const bottom = containerRect.bottom - 10;
+
+    
+    generated = 0;
+    while (generated<n){
+        let x = Math.floor(Math.random() * (right - left + 1)) + left;
+        let y = Math.floor(Math.random() * (top - bottom + 1)) + bottom;
+
+        if(add_node(div, x, y)) generated++;
+
+    }
+}
+
 
 document.addEventListener('DOMContentLoaded', function(){
 
     const graph_div = document.getElementById("graph-div");
     let selected_node = null;
+    let created_node = 0;
 
     graph_div.addEventListener('mousedown', function(event){
         //console.log("mousedown")
@@ -43,23 +85,21 @@ document.addEventListener('DOMContentLoaded', function(){
                 let id = close_to_coordinate[0]+"-"+close_to_coordinate[1];
                 //console.log(id);
                 selected_node = document.getElementById(id);
-                //console.log(selected_node)
+
+                if(event.button == 2){
+                    
+                    event.preventDefault();
+                    graph_div.removeChild(selected_node);
+                }
+                else{
+                    //console.log(selected_node)
+                    selected_node.style.cursor = 'grabbing';
+                }
+
             }
-            else{
-                const node = document.createElement('div');
-                node.classList.add('node');
-                node.id = x + "-" + y;
-                //console.log("created "+ node.id);
-                //console.log("added to array " + x + " " + y);
+            else if (event.button != 2) created_node = 1;
 
-                node.style.left = x+'px';
-                node.style.top = y+'px';
-
-                selected_node = node;
-
-                graph_div.appendChild(node);
-            }
-            selected_node.style.cursor = 'grabbing';
+            
         }
     })
 
@@ -91,9 +131,46 @@ document.addEventListener('DOMContentLoaded', function(){
             coordinates.push([x, y]);
             selected_node.style.cursor = 'grab';
         }
+        else if (created_node){
+            created_node = 0;
+            const x = event.clientX;
+            const y = event.clientY;
+    
+            const containerRect = graph_div.getBoundingClientRect();
+            const isWithinContainer = (
+                x >= containerRect.left + 10 &&
+                x <= containerRect.right - 10 &&
+                y >= containerRect.top + 10 &&
+                y <= containerRect.bottom - 10
+            );
+            if(isWithinContainer){
+                add_node(graph_div, x, y);
+            }
+        }
         selected_node = null;
 
 
     })
+
+    graph_div.addEventListener("contextmenu", function (event) {
+        event.preventDefault();
+    });
+
+    const button_clear = document.getElementById("btn-clear");
+    button_clear.addEventListener("click", function(){
+        clear_graph(graph_div);
+    })
+
+    const button_generate = document.getElementById("btn-generate");
+    const no_cities_inp = document.getElementById("cities-input");
+
+    button_generate.addEventListener("click", function () {
+        const numberOfCities = parseInt(no_cities_inp.value);
+        if (!isNaN(numberOfCities) && Number.isInteger(numberOfCities) && numberOfCities > 0) {
+            generate_cities(graph_div, numberOfCities);
+        } else {
+            alert("Please enter a valid positive integer for the number of cities.");
+        }
+    });
 
 })
